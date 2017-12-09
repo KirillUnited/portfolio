f1c96bbef50ba3ce29f70d7227b92e58ba47e309
Kirill <psykirill@rambler.ru>
2017-12-09 19:30:01 +0000

    added btn-close

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".glyphicon-remove-sign").click(function() {
+	$(".glyphicon-remove-sign, .btn-close").click(function() {
 		$(".modal-backdrop").modal("hide");
 		$(".modal").modal("hide");
 	});
